ec34acdb2d1bc0968117ec5a8574adf4ad5ce642
Noumbissi16 <noumbissistael@gmail.com>
2025-01-02 17:38:04 +0100

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="text-lg text-green-900">
+        App
+        <p>jhkjhkjhjk</p>
+        <h1>good morning</h1>
+        <s>saosaoisa</s>
+        <h2>dsskljdls</h2>
+      </div>
+    </div>
+  );
+};
+
+export default App;
