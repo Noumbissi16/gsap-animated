690080771d1da605d1eab98739f38bb2a0cc4f22
Noumbissi16 <noumbissistael@gmail.com>
2025-07-12 12:13:02 +0100

    starting back the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import gsap from "gsap";
 
 const App = () => {
+  gsap.from("#App", {
+    opacity: 0,
+    duration: 1,
+    ease: "power4.out",
+  });
+
+  gsap.to("#App", {
+    opacity: 1,
+    duration: 1,
+    ease: "power4.out",
+  });
+
   return (
     <div>
-      <div className="text-lg text-green-900">
+      <div className="text-lg text-green-900" id="App">
         App
         <p>jhkjhkjhjk</p>
         <h1>good morning</h1>
